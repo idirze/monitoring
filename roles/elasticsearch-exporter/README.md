59a8a9c55c21cf210a0bb946d10244819e997f94
Izitounene Idir <idir@MacBook-Pro.local>
2019-04-22 20:43:39 +0200

    poc

---
+++ roles/elasticsearch-exporter/README.md
@@ -0,0 +1,9 @@
+Ansible Elasticsearch Exporter Role
+============
+
+
+An Ansible Role that installs [Prometheus Elasticsearch Exporter](https://github.com/justwatchcom/elasticsearch_exporter) on Linux.
+
+## Role Variables
+
+Available variables with their default values are defined in [defaults/main.yml](defaults/main.yml).
---
+++ roles/spark-metrics/files/test.sh
@@ -0,0 +1,10 @@
+./bin/spark-submit --class org.apache.spark.examples.SparkPi \
+--conf "spark.driver.extraClassPath=/usr/hdp/current/spark2-client/metrocs-jars/metrics-core-3.1.2.jar:/usr/hdp/current/spark2-client/metrocs-jars/simpleclient-0.3.0.jar:/usr/hdp/current/spark2-client/metrocs-jars/simpleclient_dropwizard-0.3.0.jar:/usr/hdp/current/spark2-client/metrocs-jars/simpleclient_pushgateway-0.3.0.jar:/usr/hdp/current/spark2-client/metrocs-jars/spark-metrics_2.11-2.3-2.0.4.jar:/usr/hdp/current/spark2-client/metrocs-jars/collector-0.3.0.jar:/usr/hdp/current/spark2-client/metrocs-jars/snakeyaml-1.23.jar"    \
+--conf "spark.executor.extraClassPath=/usr/hdp/current/spark2-client/metrocs-jars/metrics-core-3.1.2.jar:/usr/hdp/current/spark2-client/metrocs-jars/simpleclient-0.3.0.jar:/usr/hdp/current/spark2-client/metrocs-jars/simpleclient_dropwizard-0.3.0.jar:/usr/hdp/current/spark2-client/metrocs-jars/simpleclient_pushgateway-0.3.0.jar:/usr/hdp/current/spark2-client/metrocs-jars/spark-metrics_2.11-2.3-2.0.4.jar:/usr/hdp/current/spark2-client/metrocs-jars/collector-0.3.0.jar:/usr/hdp/current/spark2-client/metrocs-jars/snakeyaml-1.23.jar"    \
+--master yarn-client     \
+--num-executors 1     \
+--driver-memory 512m  \
+--conf spark.metrics.conf=/var/lib/spark2-metrics/spark-metrics.properties    \
+--executor-memory 512m     --executor-cores 1     examples/jars/spark-examples*.jar 10
+
+
